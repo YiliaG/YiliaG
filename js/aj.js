93fe8b4c08618503d46d866567c0b960c84e5d8b
Yiliakuo <gy905155082@163.com>
2017-02-24 18:44:40 +0800

    Add:Ionic

--- js/aj.js
+++ js/aj.js
@@ -1,19 +1,32 @@
 var app = angular.module('myApp', []);
-	app.controller('costCtrl', function($scope) {
-	    $scope.quantity = 1;
-	    $scope.price = 9.99;
-	});
+app.controller('costCtrl', function ($scope) {
+    $scope.quantity = 1;
+    $scope.price = 9.99;
+});
 
-// var app = angular.module('myApp1', []);
-	app.controller('siteCtrl', function($scope, $http) {
-	  $http.get("/json/blog.json")
-	  .success(function (response) {$scope.names = response.list;});
-	});
+app.controller('siteCtrl', function ($scope, $http) {
+    $http.get("/json/blog.json")
+        .success(function (response) {
+            $scope.names = response.list;
+        });
+});
 
-	app.controller('selectCtrl', function($scope) {
-	    $scope.sites = [
-		    {site : "Google", url : "http://www.google.com"},
-		    {site : "Runoob", url : "http://www.runoob.com"},
-		    {site : "Taobao", url : "http://www.taobao.com"}
-		];
-	});
+app.controller('selectCtrl', function ($scope) {
+    $scope.sites = [
+        {site: "Google", url: "http://www.google.com"},
+        {site: "Runoob", url: "http://www.runoob.com"},
+        {site: "Taobao", url: "http://www.taobao.com"}
+    ];
+});
+
+
+// other module
+var myModule = angular.module('myModule', []);
+myModule.directive("hello", function () {
+    return {
+        restrict: 'AE',
+        scope:{},
+        template: '<div><input type="text" ng-model="userName" />{{userName}}</div>',
+        replace: true
+    }
+});
